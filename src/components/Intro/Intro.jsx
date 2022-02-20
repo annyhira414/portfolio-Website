e01cf6eb23dc30ac520efed1434532e942b99e1f
annyhira414 <annyhira@gmail.com>
2022-02-20 18:58:37 +0600

    update Intro.jsx

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,16 @@
+
+
+const Intro = () =>{
+    return (
+        <div className="i">
+            <div className="i-left">
+                <div className="i-right">
+
+                </div>
+            </div>
+        </div>
+    )
+    
+}
+
+export default Intro
